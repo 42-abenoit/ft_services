d1e536bf6db30dc21a286f262ac45cc6d6d05a4f
abenoit <abenoit@student.42.fr>
2021-02-23 17:23:29 +0100

    php bad gateway

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 minikube delete
 rm -rf ~/.minikube
-#img=$(docker images | grep gcr.io | awk '{print $3g}')
-#docker rmi $img
-#yes | docker system prune
+img=$(docker images | grep gcr.io | awk '{print $3g}')
+docker rmi $img
+yes "y" | docker system prune
---
+++ restart.sh
@@ -0,0 +1,2 @@
+bash clean.sh
+bash launch_minikube.sh
