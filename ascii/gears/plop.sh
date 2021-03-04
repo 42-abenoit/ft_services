0aec9853bdf3e08170e4e99265b1dd0b070d8dcc
abenoit <abenoit@student.42.fr>
2021-03-04 12:08:54 +0100

    non persistent ftp

--- ascii/gears/plop.sh
+++ ascii/gears/plop.sh
@@ -3,13 +3,13 @@ gears_anim () {
 echo -e "\e[93m"
 while :; do
 	for (( i=0; i<4; i++ )); do
-		cat lol_$i
-		sleep 0.15
-		echo -en "\033[12A"
-		for (( j=0; j<12; j++ )); do
+		cat plop_$i
+		sleep 0.3
+		echo -en "\033[8A"
+		for (( j=0; j<8; j++ )); do
 		 echo -e "\r\033[K"
 		done
-		echo -en "\033[12A"
+		echo -en "\033[8A"
 	done
 done
 }
@@ -18,8 +18,8 @@ gears_anim & MYSQL_PID=$!
 sleep 5
 kill $MYSQL_PID
 wait $MYSQL_PID 2> /dev/null
-echo -en "\033[12A"
-for (( j=0; j<12; j++ )); do
+echo -en "\033[8A"
+for (( j=0; j<8; j++ )); do
  echo -e "\r\033[K"
 done
-echo -en "\033[12A"
+echo -en "\033[8A"
