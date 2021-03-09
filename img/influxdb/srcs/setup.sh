a293d9a944d78c8393b8bf4ab69484031669d601
abenoit <abenoit@student.42.fr>
2021-03-09 21:04:48 +0100

    influx startup check

--- img/influxdb/srcs/setup.sh
+++ img/influxdb/srcs/setup.sh
@@ -2,15 +2,21 @@ influxd &
 
 sleep 0.5
 
-echo -n "Waiting for influxd to launch"
-cmp=$(pgrep influxdb | wc -l)
-while [ ! $cmp -eq 0 ]
+#echo -n "Waiting for influxd to launch"
+#cmp=$(pgrep influxdb | wc -l)
+#while [ ! $cmp -eq 0 ]
+#do
+#cmp=$(pgrep influxdb | wc -l)
+#sleep 0.2
+#echo -n .
+#done
+#echo
+
+until [ ! $(influx -host 127.0.0.1 -port 8086 -execute 'exit') ]
 do
-cmp=$(pgrep influxdb | wc -l)
 sleep 0.2
 echo -n .
 done
-echo
 
 influx -host 127.0.0.1 -port 8086 <<- EOF
 create database telegraf;
