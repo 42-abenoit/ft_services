9a8a77917b46fb6de188a8e8998117faa7decaa6
abenoit <abenoit@student.42.fr>
2021-03-17 00:03:12 +0100

    small layout fixes

--- control_scripts/help.sh
+++ control_scripts/help.sh
@@ -2,7 +2,7 @@
 
 STYLE="\e[1m\e[36m"
 
-echo -e "$STYLE"
+echo -en "$STYLE"
 
 echo -e "./control_scripts/access.sh POD"
 echo -e "Open a shell terminal into selected pod."
@@ -40,16 +40,16 @@ echo -e "\n./control_scripts/restart.sh"
 echo -e "Delete minikube cluster and temporary files"
 echo -e "and restart project installation."
 
-echo -e ".\n/control_scripts/links.sh"
+echo -e "\n./control_scripts/links.sh"
 echo -e "Display services links."
 
-echo -e ".\n/control_scripts/passwords.sh"
+echo -e "\n./control_scripts/passwords.sh"
 echo -e "Display services credentials."
 
 echo -e "\n./control_scripts/help.sh"
 echo -e "Display this help message."
 
-echo -e "Note: Wordpress and PhpMyAdmin are accessed"
+echo -e "\nNote: Wordpress and PhpMyAdmin are accessed"
 echo -e "with "wp" and "pma" abbreviations"
 
-echo -e "\e[0m"
+echo -en "\e[0m"
